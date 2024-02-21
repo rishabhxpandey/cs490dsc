b476a603e04aceb466f302035efd1d5ec239fd35
Supriya Dixit <supriyadixit61@gmail.com>
2024-02-21 17:04:22 -0500

    edits to job file to execute py script

--- job.sh
+++ job.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # FILENAME: job.sh
 #SBATCH --output=/home/dixit22/cs490dsc/joboutput/myjob.out
-#SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=4
 #SBATCH --time=04:00:00
 #SBATCH --job-name cifar-resnet
 
 module load anaconda
+module load cuda
 module load use.own
 source activate d22env
 echo -e "module loaded"
+echo $CUDA_VISIBLE_DEVICES
 
 cd /home/dixit22/cs490dsc
-jupyter execute cifar10_resnet18.ipynb
+#jupyter execute cifar10_resnet18.ipynb
+
+python -u cifar10_resnet18.py
