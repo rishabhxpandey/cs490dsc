2c4985cc88569735f98176d4a93cdcc1381f2d61
Sai Coumar <sai.c.coumar1@gmail.com>
2024-02-24 15:36:28 -0500

    Add files via upload

--- setup_env.sh
+++ setup_env.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # FILENAME: job.sh
 #SBATCH --output=myjob.out
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=4
 #SBATCH --time=04:00:00
 #SBATCH --job-name cifar-resnet
-#SBATCH --output ./out.out
-#SBATCH --error ./err.err
-#SBATCH --job-name testing_models
 
-module purge
 module load anaconda
 module load use.own
+conda env remove --name d22env
+conda create --name d22env python=3.11 jupyter pytorch torchvision matplotlib pandas -y
 source activate d22env
 conda info --envs
 echo -e "module loaded"
 
 
-python -u cifar_resnet18.py
+
---
+++ view_output.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+	cat out.out
+	sleep 5
+done
+
