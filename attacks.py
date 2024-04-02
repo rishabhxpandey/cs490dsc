b7c850aed3afb794bc9e5e49033c53f5296b04df
Sai Coumar <sai.c.coumar1@gmail.com>
2024-04-02 11:07:59 -0400

    Add files via upload

--- attacks.py
+++ attacks.py
@@ -58,11 +58,12 @@ def deepfool_attack(image, model, overshoot=0.02, max_iterations=50):
     # Get the actual predictions from the model
     output_actual, _ = model(x)
     # Probability of the label's classification
-    label_probability = output_actual[0][7].item()
+    # print(output_actual)
+    
 
     # Actual label value (number)
     _, label_actual = torch.max(output_actual.data, 1)
-
+    label_probability = output_actual[0][label_actual].item()
     # Store reference of original image
     original_img = copy.deepcopy(image)
     # Initialize weights w
@@ -154,6 +155,7 @@ def pgd_attack(image, model, init_pred, epsilon, alpha=2,  max_iterations=50):
 
         # Extract gradient
         sign_data_grad = image.grad.data.sign()
+        # print(sign_data_grad.size())
 
         # Perturb image
         perturbed_image = image  + alpha * sign_data_grad
@@ -163,4 +165,146 @@ def pgd_attack(image, model, init_pred, epsilon, alpha=2,  max_iterations=50):
         perturbed_image = torch.clamp(perturbed_image + eta, min=0, max=255)
 
     return output_final, perturbed_image
- 
+
+def nes_attack(image, model, init_pred, init_labels, epsilon, alpha=2,  max_iterations=1):
+    """
+    Author: Sai Coumar
+    Description: Perturbs an image using the Natural Evolution Strategies (Finite Differences Variant) attack
+    Attack Type: Score-Based Black Box
+
+    Parameters:
+    - image: The pytorch tensor of an image to perturb
+    - model: The classifier model to attack
+    - init_pred: True classified label given by the model before any attacks
+    - epsilon: A hyperparameter that defines the epsilon-ball threshold that the perturbed image
+    must stay confined to in order to retain percievability
+    - alpha: Step size hyperparameter to control the magnitude of perturbation
+    - max_iterations: Hyperparameter to limit resources to finite value  
+
+    Returns:
+    - Perturbed image
+
+    Literature:
+    - https://arxiv.org/pdf/1804.08598.pdf
+    """
+    def create_multivariate_gaussian(mean, cov_matrix):
+        # Create a MultivariateNormal distribution with the specified mean and covariance matrix
+        mv_normal = torch.distributions.multivariate_normal.MultivariateNormal(mean, cov_matrix)
+        # Sample from the distribution
+        sample = mv_normal.sample()
+        return sample
+        
+    def nes_estimation(sign_data_grad_actual, model, label_actual, sigma, n, image):
+        N = image.size()[2]
+
+        g = torch.zeros(1, 1, N, N)
+        g = g.to(device)
+        # print(g.size())
+        for _ in range(n):
+            
+            ui = create_multivariate_gaussian(torch.zeros(N), torch.eye(N)).unsqueeze(0).unsqueeze(0)
+            ui = ui.to(device)
+
+            p_plus = model(image + sigma * ui)[0][0][label_actual].item()
+            p_minus = model(image - sigma * ui)[0][0][label_actual].item()
+
+            g += p_plus * ui
+            g -= p_minus * ui
+            
+            print("diff: ", torch.norm((sign_data_grad_actual - g).view(-1)).item(), " ,prob +: ", p_plus, " ,prob -: ", p_minus)
+          
+        return (1 / (2 * n * sigma)) * g
+
+    
+    # # Note: other examples may use alpha and epsilon divided by 255 
+    # # because they normalize pixels to 1.
+    # # Since normalization wasn't used we use integers 
+    perturbed_image = image
+    output_final = None
+    
+
+    # max_iterations = 1
+    for _ in range(max_iterations):
+        # Predict on perturbed image
+        output, _ = model(perturbed_image)
+        output_final = output
+
+        _, label_actual = torch.max(output.data, 1)
+        label_probability = output[0][init_pred].item()
+        # print(label_probability)
+        loss = F.cross_entropy(output, init_pred)
+        model.zero_grad()
+        loss.backward() 
+
+        # Extract gradient
+        sign_data_grad_actual = image.grad.data.sign()
+        # Extract gradient
+        sign_data_grad = nes_estimation(sign_data_grad_actual, model, init_pred, sigma=0.001, n=100, image = perturbed_image)
+        sign_data_grad = sign_data_grad.to(device)
+        # print(torch.norm((sign_data_grad_actual - sign_data_grad).view(-1)))
+        print(label_actual[0].item(), init_pred[0].item(), label_probability, " ,diff: ", torch.norm((sign_data_grad_actual - sign_data_grad).view(-1)).item())
+
+        # Perturb image
+        perturbed_image = image + alpha * sign_data_grad
+        
+        # Clipping to epislon ball
+        eta = torch.clamp(perturbed_image - image, min=-epsilon, max=epsilon)
+        perturbed_image = torch.clamp(perturbed_image + eta, min=0, max=255)
+        # print(perturbed_image)
+
+    return output_final, perturbed_image
+    
+    # def nes_estimation(sign_data_grad_actual, model, label_actual, labels, sigma, n, image):
+    #     N = image.size()[2]
+    #     grads = []
+    #     final_losses = []
+
+
+    #     g = torch.zeros(1, 1, N, N)
+    #     g = g.to(device)
+    #     # print(g.size())
+    #     for _ in range(n):
+            
+    #         ui = create_multivariate_gaussian(torch.zeros(N), torch.eye(N)).unsqueeze(0).unsqueeze(0)
+    #         ui = ui.to(device)
+    #         # print(model(image + sigma * ui)[0][0])
+    #         print(ui.size())
+    #         noise = torch.cat([ui, -ui], dim=0)
+    #         print(noise.size())
+    #         eval_points = image + sigma * noise
+    #         output, _ = model(eval_points)
+    #         print(output, labels)
+    #         loss = F.cross_entropy(output, labels)
+    #         print(loss)
+    #         losses_tiled = loss.view(1, 1, 1, 1).expand(1,1,N,N)
+    #         print(losses_tiled)
+    #         grads.append(losses_tiled * noise)
+    #         # g = (grad_plus - grad_minus) * ui
+    #         # grads.append(g)
+    #         # print("diff: ", torch.norm((sign_data_grad_actual - g).view(-1)).item(), " ,prob +: ", grad_plus, " ,prob -: ", grad_minus)
+            
+    #     g = torch.sum(torch.stack(grads), dim=0)
+    #     return (1 / (2 * n * sigma)) * g
+
+    # perturbed_image = image
+    # output_final = None
+    # for _ in range(max_iterations):
+    #     # Predict on perturbed image
+    #     output, _ = model(perturbed_image)
+    #     output_final = output
+    #     # _, pred = torch.max(output.data, 1)
+
+    #     # Compare loss of true prediction vs outputted prediction
+        
+    #     # Extract gradient
+    #     sign_data_grad = nes_estimation(model, init_pred, sigma=0.001, n=50, image = perturbed_image)
+    #     # print(sign_data_grad.size())
+
+    #     # Perturb image
+    #     perturbed_image = image  + alpha * sign_data_grad
+        
+    #     # Clipping to epislon ball
+    #     eta = torch.clamp(perturbed_image - image, min=-epsilon, max=epsilon)
+    #     perturbed_image = torch.clamp(perturbed_image + eta, min=0, max=255)
+
+    # return output_final, perturbed_image
